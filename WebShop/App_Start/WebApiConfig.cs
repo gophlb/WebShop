3385841e628f046164e8fccf971b56348034bb8e
gophlb <gophlb@hotmail.com>
2015-05-18 00:12:03 +0200

    - Added webapi actions for Product - Added test page for webapi (webapitest.html) - Added page for webapi overview (webapioverview.html)

--- WebShop/App_Start/WebApiConfig.cs
+++ WebShop/App_Start/WebApiConfig.cs
@@ -16,8 +16,8 @@ namespace WebShop
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{reference}",
+                defaults: new { reference = RouteParameter.Optional }
             );
         }
     }
---
+++ WebShop/Controllers/api/WebApiProductController.cs
@@ -0,0 +1,45 @@
+﻿using BLL;
+using Core;
+using System.Collections.Generic;
+using System.Web.Http;
+
+
+namespace WebShop.WebApiControllers
+{
+    public class WebApiProductController : ApiController
+    {
+        private const int PRODUCTS_PER_PAGE = 10;
+        private IProductManager productManager;
+
+        public WebApiProductController(IProductManager productManager)
+        {
+            this.productManager = productManager;
+        }
+
+
+        public List<ProductViewModel> GetAllProducts()
+        {
+            List<ProductViewModel> products = productManager.GetAllProducts();
+            
+            return products;
+        }
+
+
+        public List<ProductViewModel> GetPagedProducts(int page)
+        {
+            List<ProductViewModel> products = productManager.GetProducts(page, PRODUCTS_PER_PAGE);
+
+            return products;
+        }
+
+
+        public IHttpActionResult GetProduct(string reference)
+        {
+            ProductViewModel product = productManager.GetProductByReference(reference);
+            
+            if (product == null) { return NotFound(); }
+            
+            return Ok(product);
+        }
+    }
+}
